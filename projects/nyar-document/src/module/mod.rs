1c854b5da36c04c9f156dcd40d1a5a1eba970902
Aster <aster@vers.site>
2017-06-15 16:01:36 +0000

    🐤 Support custom module name and module summary

--- projects/nyar-document/src/module/mod.rs
+++ projects/nyar-document/src/module/mod.rs
@@ -1,9 +1,11 @@
+use std::ops::AddAssign;
 use askama::Template;
 
-#[derive(Template)]
-#[template(path = "module.html")]
+#[derive(Debug, Template)]
+#[template(path = "module.html.jinja2")]
 pub struct DocumentModule {
-    namepath: Vec<String>,
+    namespace: Vec<String>,
+    name: String,
     /// html summary
     summary: String,
     modules: Vec<DocumentModule>,
@@ -12,18 +14,48 @@ pub struct DocumentModule {
     structures: Vec<DocumentStructure>,
 }
 
+impl DocumentModule {
+    pub fn new<S: ToString>(name: S) -> Self {
+        Self {
+            namespace: vec![],
+            name: name.to_string(),
+            summary: "".to_string(),
+            modules: vec![],
+            types: vec![],
+            interfaces: vec![],
+            structures: vec![],
+        }
+    }
+    pub fn with_summary<S: ToString>(self, summary: S) -> Self {
+        Self {
+            summary: summary.to_string(),
+            ..self
+        }
+    }
+}
+
+impl AddAssign<DocumentModule> for DocumentModule {
+    fn add_assign(&mut self, rhs: DocumentModule) {
+        self.modules.push(rhs);
+    }
+}
+
+
+#[derive(Debug)]
 pub struct DocumentType {
     namepath: Vec<String>,
     /// html summary
     summary: String,
 }
 
+#[derive(Debug)]
 pub struct DocumentInterface {
     namepath: Vec<String>,
     /// html summary
     summary: String,
 }
 
+#[derive(Debug)]
 pub struct DocumentStructure {
     namepath: Vec<String>,
     /// html summary
--- projects/nyar-document/tests/main.rs
+++ projects/nyar-document/tests/main.rs
@@ -1,4 +1,24 @@
+use std::fs::File;
+use std::io::Write;
+use nyar_document::DocumentModule;
+use askama::Template;
+
 #[test]
 fn ready() {
     println!("it works!")
+}
+
+
+#[test]
+fn main() {
+    let mut hello = DocumentModule::new("io"); // instantiate your struct
+    let new1 = DocumentModule::new("new1").with_summary("new io");
+    let new2 = DocumentModule::new("new2").with_summary("new io");
+    let new3 = DocumentModule::new("new3").with_summary("new io");
+    hello += new1;
+    hello += new2;
+    hello += new3;
+    std::fs::create_dir_all("target/std").unwrap();
+    let mut out = File::create("target/std/io.html").unwrap();
+    out.write_all(hello.render().unwrap().as_bytes()).unwrap();
 }
