3c2adb077b318dc7e257a09a19476ae7cd6810a3
Aster <aster@vers.site>
2017-06-18 00:31:02 +0000

    🎀 Add colored link

--- projects/nyar-document/src/structures/mod.rs
+++ projects/nyar-document/src/structures/mod.rs
@@ -1,9 +1,30 @@
-
-
 #[derive(Debug)]
 pub struct DocumentStructure {
     namepath: Vec<String>,
     name: String,
     /// html summary
     summary: String,
+}
+
+
+impl DocumentStructure {
+    pub fn new<S: ToString>(name: S) -> Self {
+        Self {
+            namepath: vec![],
+            name: name.to_string(),
+            summary: "".to_string(),
+        }
+    }
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+    pub fn get_summary(&self) -> &str {
+        &self.summary
+    }
+    pub fn with_summary<S: ToString>(self, summary: S) -> Self {
+        Self {
+            summary: summary.to_string(),
+            ..self
+        }
+    }
 }
